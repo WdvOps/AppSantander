5637a7daab5c3133aee5e4ff4518fe742a5545c0
WdvOps <dvops@outlook.com>
2021-05-27 09:04:06 -0300

    APIs created

---
+++ src/main/java/com/project/appSantander/controller/StockController.java
@@ -0,0 +1,70 @@
+package com.project.appSantander.controller;
+
+
+import com.project.appSantander.model.dto.StockDTO;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/stock")
+public class StockController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_PROBLEM_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
+    public ResponseEntity<StockDTO> save(@Valid @RequestBody StockDTO dto) {
+        return ResponseEntity.ok(dto);
+
+    }
+    @PutMapping(consumes = MediaType.APPLICATION_PROBLEM_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
+    public ResponseEntity<StockDTO> update(@Valid @RequestBody StockDTO dto) {
+        return ResponseEntity.ok(dto);
+
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StockDTO>> findAll() {
+        List<StockDTO> list = new ArrayList<>();
+        StockDTO stock1 = new StockDTO();
+        stock1.setId(1L);
+        stock1.setName("Magazine Luiza");
+        stock1.setPrice(100D);
+        stock1.setVariation(10D);
+        stock1.setDate(LocalDate.now());
+
+
+
+        return ResponseEntity.ok(list);
+
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> findById(@PathVariable Long id) {
+
+        List<StockDTO> list = new ArrayList<>();
+        StockDTO stock1 = new StockDTO();
+        stock1.setId(1L);
+        stock1.setName("Magazine Luiza");
+        stock1.setPrice(100D);
+        stock1.setVariation(10D);
+        stock1.setDate(LocalDate.now());
+
+        StockDTO stock2 = new StockDTO();
+        stock2.setId(2L);
+        stock2.setName("Lojas Americanas");
+        stock2.setPrice(150D);
+        stock2.setVariation(5D);
+        stock2.setDate(LocalDate.now());
+        list.add(stock1);
+        list.add(stock2);
+
+        StockDTO dtoSelect = list.stream().filter(x -> x.getId().compareTo(id) == 0).findFirst().get();
+
+        return  ResponseEntity.ok(dtoSelect);
+    }
+
+}
---
+++ src/test/java/com/project/appSantander/AppSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.appSantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
