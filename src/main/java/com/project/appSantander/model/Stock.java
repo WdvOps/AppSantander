06d0733540c914638aec6d54f8a46a5d7b010db3
WdvOps <dvops@outlook.com>
2021-05-29 18:56:12 -0300

    feat: added data base

--- src/main/java/com/project/appSantander/model/Stock.java
+++ src/main/java/com/project/appSantander/model/Stock.java
@@ -1,6 +1,11 @@
 package com.project.appSantander.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
